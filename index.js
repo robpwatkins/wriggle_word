9aa00076be7637e0860182d1d49cf3965f08720a
robpwatkins <robpwatkins@gmail.com>
2023-06-19 17:57:32 -0500

    the basics are in place

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const container = document.querySelector('.container');
+const gameboard = document.querySelector('.gameboard');
+const currentWord = document.querySelector('.current-word');
 
 const leadCoords = {
-  row: 2,
-  column: 5
+  row: 10,
+  column: 6
 };
 
 let lead = document.createElement('span');
-lead.innerHTML = 'a';
+lead.innerHTML = 'y';
 
 const { row, column } = leadCoords;
 lead.classList.add('letter', 'active');
 lead.style.gridArea = `${row}/${column}`;
 
-container.insertAdjacentElement('beforeend', lead);
+gameboard.insertAdjacentElement('beforeend', lead);
 
 const letters = [lead];
 let direction;
@@ -25,8 +26,8 @@ window.addEventListener('keyup', ({ code }) => {
   else if (direction === 'down') leadCoords.row++;
   else leadCoords.column--;
 
-  const newCoords = `${leadCoords.row}/${leadCoords.column}`;
-  const newLetter = document.querySelector(`[data-coords="${newCoords}"]`);
+  const newCoords = `${leadCoords.row} / ${leadCoords.column}`;
+  const newLetter = document.querySelector(`[style="grid-area: ${newCoords};"]`);
   
   if (newLetter) {
     if (direction === 'up') leadCoords.row--;
@@ -34,17 +35,21 @@ window.addEventListener('keyup', ({ code }) => {
     else if (direction === 'down') leadCoords.row++;
     else leadCoords.column--;
 
-    delete newLetter.dataset.coords;
     newLetter.classList.add('active');
     letters.push(newLetter);
+
+    if (newLetter.innerHTML === '_') currentWord.innerHTML = '<h3>_</h3>';
+    else {
+      const newLetterHeading = `<h3>${newLetter.innerHTML}</h3>`;
+      if (currentWord.innerHTML.includes('_')) currentWord.innerHTML = newLetterHeading;
+      else currentWord.insertAdjacentHTML('beforeend', newLetterHeading);
+    }
   }
 
   letters.forEach((letter, idx) => {
     if (idx !== (letters.length - 1)) {
       const { gridArea: nextCoords } = letters[idx + 1].style;
       letter.style.gridArea = nextCoords;
-    } else {
-      letter.style.gridArea = `${leadCoords.row} / ${leadCoords.column}`;
-    }
+    } else letter.style.gridArea = `${leadCoords.row} / ${leadCoords.column}`;
   });
 })
