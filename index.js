2b9bd03d45c5e189e1cdf9ccb330ac6f62a8c068
robpwatkins <robpwatkins@gmail.com>
2023-06-19 20:38:54 -0500

    setting space orientation like a champ

--- index.js
+++ index.js
@@ -6,50 +6,71 @@ const leadCoords = {
   column: 6
 };
 
-let lead = document.createElement('span');
+const lead = document.createElement('span');
 lead.innerHTML = 'y';
 
 const { row, column } = leadCoords;
 lead.classList.add('letter', 'active');
-lead.style.gridArea = `${row}/${column}`;
+lead.style.gridArea = `${row} / ${column}`;
 
 gameboard.insertAdjacentElement('beforeend', lead);
 
 const letters = [lead];
-let direction;
 
-window.addEventListener('keyup', ({ code }) => {
-  direction = code.replace('Arrow', '').toLowerCase();
+window.addEventListener('keyup', handleKeyUp);
 
+function handleKeyUp({ code }) {
+  const direction = code.replace('Arrow', '').toLowerCase();
+  updateCoords(direction);
+
+  const newLetter = document
+    .querySelector(`[style="grid-area: ${leadCoords.row} / ${leadCoords.column};"]`);
+  
+  if (newLetter) addNewLetter(newLetter, direction);
+
+  updateLetterPositions();
+};
+
+function updateCoords(direction) {
   if (direction === 'up') leadCoords.row--;
   else if (direction === 'right') leadCoords.column++;
   else if (direction === 'down') leadCoords.row++;
-  else leadCoords.column--;
+  else if (direction === 'left') leadCoords.column--;
+};
 
-  const newCoords = `${leadCoords.row} / ${leadCoords.column}`;
-  const newLetter = document.querySelector(`[style="grid-area: ${newCoords};"]`);
-  
-  if (newLetter) {
-    if (direction === 'up') leadCoords.row--;
-    else if (direction === 'right') leadCoords.column++;
-    else if (direction === 'down') leadCoords.row++;
-    else leadCoords.column--;
+function addNewLetter(newLetter, direction) {
+  updateCoords(direction);
 
-    newLetter.classList.add('active');
-    letters.push(newLetter);
+  newLetter.classList.add('active');
+  letters.push(newLetter);
 
-    if (newLetter.innerHTML === '_') currentWord.innerHTML = '<h3>_</h3>';
-    else {
-      const newLetterHeading = `<h3>${newLetter.innerHTML}</h3>`;
-      if (currentWord.innerHTML.includes('_')) currentWord.innerHTML = newLetterHeading;
-      else currentWord.insertAdjacentHTML('beforeend', newLetterHeading);
-    }
+  if (newLetter.innerHTML === '_') currentWord.innerHTML = '<h3>_</h3>';
+  else {
+    const newLetterHeading = `<h3>${newLetter.innerHTML}</h3>`;
+    if (currentWord.innerHTML.includes('_')) currentWord.innerHTML = newLetterHeading;
+    else currentWord.insertAdjacentHTML('beforeend', newLetterHeading);
   }
+};
 
+function updateLetterPositions() {
   letters.forEach((letter, idx) => {
+    const { gridArea: nextCoords } = (idx !== (letters.length - 1))
+      ? letters[idx + 1].style
+      : {};
+
+    if (letter.innerHTML === '_') setOrientation(letter, nextCoords);
     if (idx !== (letters.length - 1)) {
-      const { gridArea: nextCoords } = letters[idx + 1].style;
       letter.style.gridArea = nextCoords;
     } else letter.style.gridArea = `${leadCoords.row} / ${leadCoords.column}`;
   });
-})
+};
+
+function setOrientation(letter, nextCoords) {
+  const [currentRow] = letter.style.gridArea.split(' / ');
+  const [nextRow] = nextCoords
+    ? nextCoords.split(' / ')
+    : Object.values(leadCoords).map(coord => coord.toString());
+
+  const orientation = (currentRow === nextRow) ? 'horizontal' : 'vertical';
+  letter.setAttribute('data-orientation', orientation);
+};
